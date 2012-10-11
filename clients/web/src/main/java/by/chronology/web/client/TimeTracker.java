4e164ccd2a3200f22ce1874a20777fea2eb263b3
Tsimafei Shchytkavets <timoxino@gmail.com>
2012-10-11 22:45:19 +0300

    Maven conf. for GWT app. was fixed

---
+++ clients/web/src/main/java/by/chronology/web/client/TimeTracker.java
@@ -0,0 +1,15 @@
+package by.chronology.web.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author Tsimafei Shchytkavets
+ *         Creation Date: 10/8/12
+ */
+public class TimeTracker implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+
+    }
+}
