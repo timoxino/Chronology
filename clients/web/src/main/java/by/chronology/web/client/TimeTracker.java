8e125493b32d9d52de79cf717507016941110ee6
Tsimafei Shchytkavets <timoxino@gmail.com>
2012-10-16 23:22:30 +0300

    Simple button added to main page

--- clients/web/src/main/java/by/chronology/web/client/TimeTracker.java
+++ clients/web/src/main/java/by/chronology/web/client/TimeTracker.java
@@ -1,6 +1,8 @@
 package by.chronology.web.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * @author Tsimafei Shchytkavets
@@ -10,6 +12,7 @@ public class TimeTracker implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-
+        RootPanel rootPanel = RootPanel.get();
+        rootPanel.add(new Button("Do nothing!!!"));
     }
 }
