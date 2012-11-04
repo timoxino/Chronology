7a362805f2d81d5d1ef554e0dbff4572414c2cbd
Tsimafei Shchytkavets <timoxino@gmail.com>
2012-11-04 16:19:29 +0300

    Implemented TimeTagDao

---
+++ domains/core/core-dao/src/main/java/by/chronology/core/dao/TimeTagDao.java
@@ -0,0 +1,23 @@
+package by.chronology.core.dao;
+
+import by.chronology.common.dao.GenericDao;
+import by.chronology.core.model.TimeTag;
+
+/**
+ * Data Access Object for manipulating with <code>TimeTag</code> objects.
+ *
+ * @author Tsimafei Shchytkavets
+ *         Creation Date: 11/4/12
+ */
+public class TimeTagDao extends GenericDao<TimeTag>
+{
+    /**
+     * Instantiate DAO based on <code>class</code>.
+     *
+     * @param c1ass Class will be used by DAO
+     */
+    public TimeTagDao(Class<TimeTag> c1ass)
+    {
+        super(c1ass);
+    }
+}
