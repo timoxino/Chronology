c0dd6469722b1bc22fcbf16f4c7c5183029108a1
Tsimafei Shchytkavets <timoxino@gmail.com>
2012-11-09 21:05:10 +0300

    TimeTagDao should extend and parametrize GenericDao

--- domains/core/core-dao/src/main/java/by/chronology/core/dao/TimeTagDao.java
+++ domains/core/core-dao/src/main/java/by/chronology/core/dao/TimeTagDao.java
@@ -1,11 +1,14 @@
 package by.chronology.core.dao;
 
+import by.chronology.common.dao.GenericDao;
+import by.chronology.core.model.TimeTag;
+
 /**
  * Interface for DAOs working with <code>TimeTag</code> entities.
  *
  * @author Tsimafei Shchytkavets
  *         Creation Date: 11/9/12
  */
-public interface TimeTagDao
+public interface TimeTagDao extends GenericDao<TimeTag>
 {
 }
