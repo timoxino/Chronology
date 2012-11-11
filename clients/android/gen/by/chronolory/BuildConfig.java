dfdf45d7f43ece983ff383a293202b1b48f3f481
Tsimafei Shchytkavets <timoxino@gmail.com>
2012-11-11 20:34:01 +0300

    Implemented Android client for REST service

---
+++ clients/android/gen/by/chronolory/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package by.chronolory;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ clients/android/src/main/java/by/chronolory/TimeTagManager.java
@@ -0,0 +1,87 @@
+package by.chronolory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11.11.12
+ * Time: 15:20
+ * To change this template use File | Settings | File Templates.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class TimeTagManager extends Activity implements OnClickListener {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+        findViewById(R.id.my_button).setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View arg0) {
+        Button b = (Button)findViewById(R.id.my_button);
+        b.setClickable(false);
+        new LongRunningGetIO().execute();
+    }
+
+
+
+    private class LongRunningGetIO extends AsyncTask <Void, Void, String> {
+        protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+            InputStream in = entity.getContent();
+            StringBuffer out = new StringBuffer();
+            int n = 1;
+            while (n>0) {
+                byte[] b = new byte[4096];
+                n =  in.read(b);
+                if (n>0) out.append(new String(b, 0, n));
+            }
+            return out.toString();
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpGet httpGet = new HttpGet("http://www.cheesejedi.com/rest_services/get_big_cheese.php");
+            String text = null;
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+                HttpEntity entity = response.getEntity();
+                text = getASCIIContentFromEntity(entity);
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+            return text;
+        }
+        protected void onPostExecute(String results) {
+            if (results!=null) {
+                EditText et = (EditText)findViewById(R.id.my_edit);
+                et.setText(results);
+            }
+            Button b = (Button)findViewById(R.id.my_button);
+            b.setClickable(true);
+        }
+    }
+}
