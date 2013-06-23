de77e003750a3bbf27153bc2ac67cf3d94634a68
Tsimafei Shchytkavets <timoxino@gmail.com>
2013-06-23 21:27:12 +0300

    Added stub implementation of RESTful service

---
+++ services/REST/src/main/java/by/chronology/service/rest/TimeTagController.java
@@ -0,0 +1,51 @@
+package by.chronology.service.rest;
+
+import by.chronology.core.model.TimeTag;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * RESTful service that provides CRUD functionality related to <code>TimeTag</code> entity.
+ *
+ * @author Tsimafei Shchytkavets
+ *         Creation Date: 6/9/13
+ */
+@Controller
+@RequestMapping("/timeTags")
+public class TimeTagController
+{
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = {"Accept=application/json"})
+    public @ResponseBody TimeTag getTimeTags(@PathVariable long id)
+    {
+        // TODO: stub implementation
+        return new TimeTag();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void putTimeTag(@PathVariable long id, @RequestBody TimeTag timeTag)
+    {
+        // TODO: stub implementation
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTimeTag(@PathVariable long id)
+    {
+        // TODO: stub implementation
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody TimeTag createTimeTag(@RequestBody TimeTag timeTag)
+    {
+        // TODO: stub implementation
+        return new TimeTag();
+    }
+}
