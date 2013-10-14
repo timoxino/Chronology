69daa70909be79ec4a51ae7ddd3b012f7235a0f6
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2013-10-14 16:26:24 +0300

    Implemented activity for notification area

---
+++ clients/web/src/main/java/by/chronology/web/client/common/layout/NotificationActivity.java
@@ -0,0 +1,43 @@
+package by.chronology.web.client.common.layout;
+
+import by.chronology.web.client.component.AlertComponent;
+import by.chronology.web.client.event.BaseEventHandler;
+import by.chronology.web.client.event.ShowAlertEvent;
+
+import com.google.gwt.activity.shared.AbstractActivity;
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * Control 'Notification area' with a life cycle managed by an ActivityManager.
+ * 
+ * @author Tsimafei Shchytkavets
+ */
+@Singleton
+public class NotificationActivity extends AbstractActivity implements BaseEventHandler<ShowAlertEvent>
+{
+    PlaceController placeController;
+    AlertComponent alertComponent;
+
+    @Inject
+    public NotificationActivity(PlaceController controller, AlertComponent alert)
+    {
+        placeController = controller;
+        alertComponent = alert;
+    }
+
+    @Override
+    public void start(AcceptsOneWidget panel, EventBus eventBus)
+    {
+        panel.setWidget(alertComponent);
+    }
+
+    @Override
+    public void handleEvent(ShowAlertEvent event)
+    {
+        alertComponent.show(event.getAlertType(), event.getHeading(), event.getMessage());
+    }
+}
