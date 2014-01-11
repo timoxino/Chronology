19c0f41f5732b7428f863c5bc20e08efb9717f0e
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2014-01-11 23:01:03 +0300

    Tomcat port was made configurable in .properties file

--- services/REST/src/main/java/by/chronology/service/RestSpringConfiguration.java
+++ services/REST/src/main/java/by/chronology/service/RestSpringConfiguration.java
@@ -1,22 +1,25 @@
 package by.chronology.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Java-based Spring configuration.
  *
  * @author Tsimafei_Shchytkavets
  */
+@PropertySource("classpath:environment.properties")
 @Configuration
 public class RestSpringConfiguration
 {
     @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
+    public EmbeddedServletContainerFactory servletContainer(@Value("${tomcat.port}") String port) {
         final TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-        factory.setPort(9090);
+        factory.setPort(Integer.valueOf(port));
         return factory;
     }
 }
