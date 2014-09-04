71d427cc1ee362389558a3af56317e2e3c19b913
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2014-09-04 16:22:30 +0300

    Updated PK declaration signature

--- domains/core/core-model/src/main/sql/core-ddl.sql
+++ domains/core/core-model/src/main/sql/core-ddl.sql
@@ -6,6 +6,5 @@ CREATE TABLE TIME.TIME_TAG
   TIME_TAG_DESC varchar(255) DEFAULT NULL COMMENT 'String description giving more information about Time Tag.',
   TIME_TAG_NAME varchar(255) NOT NULL COMMENT 'Short name of the TimeTag.',
   TIME_TAG_TS datetime NOT NULL COMMENT 'The date and time this tag was created.',
-  PRIMARY KEY (TIME_TAG_KEY),
-  UNIQUE KEY TIME_TAG_KEY (TIME_TAG_KEY)
+  CONSTRAINT TIME_TAG_PK PRIMARY KEY (TIME_TAG_KEY)
 ) COMMENT = 'Table for the most base entity represents tag of the time with name and description as a string.';
