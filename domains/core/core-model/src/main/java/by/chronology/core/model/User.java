0a1b8eda6f3fa2248c8284bb7ab080135117e5ef
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2014-09-19 18:18:18 +0300

    User entity has been mapped

---
+++ domains/core/core-model/src/main/java/by/chronology/core/model/User.java
@@ -0,0 +1,57 @@
+package by.chronology.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity that represents registered user and includes essential only info about him.
+ *
+ * @author Tsimafei_Shchytkavets
+ */
+@Entity
+@Table(name = "USER", schema = "TIME")
+public class User
+{
+    private Long id;
+    private String email;
+    private String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "USER_KEY", unique = true, nullable = false)
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    @Column(name = "EMAIL", nullable = false)
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    @Column(name = "PASSWORD", nullable = false)
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+}
