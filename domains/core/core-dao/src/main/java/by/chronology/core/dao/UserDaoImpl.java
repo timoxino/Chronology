ea54df44700a344b58d00faa8043c89aedc6698a
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2014-12-10 17:38:05 +0300

    Annotate UserDao as a Repository

--- domains/core/core-dao/src/main/java/by/chronology/core/dao/UserDaoImpl.java
+++ domains/core/core-dao/src/main/java/by/chronology/core/dao/UserDaoImpl.java
@@ -2,12 +2,14 @@ package by.chronology.core.dao;
 
 import by.chronology.common.dao.impl.GenericDaoImpl;
 import by.chronology.core.model.User;
+import org.springframework.stereotype.Repository;
 
 /**
  * An implementation of {@link by.chronology.core.dao.UserDao} interface.
  *
  * @author Tsimafei_Shchytkavets
  */
+@Repository(value = "userDao")
 public class UserDaoImpl extends GenericDaoImpl<User> implements UserDao
 {
     /**
